b76e1d436ab2f950b87be71e68c517f87a472b6b
B1ackGhost <smirnovvlad313@gmail.com>
2023-05-04 22:45:02 +0300

    Лабораторная работа №8

---
+++ Lab8/SQLQuery5.sql
@@ -0,0 +1,3 @@
+CREATE VIEW [Дисциплины](Код, Наименование_дисциплины, Код_кафедры)
+AS SELECT TOP 100 * FROM SUBJECT
+ORDER BY SUBJECT
---
+++ Lab8/SQLQuery6.sql
@@ -0,0 +1,4 @@
+CREATE VIEW [Количество_кафедр] WITH SCHEMABINDING
+AS SELECT fc.FACULTY_NAME[Факультет], COUNT(*)[Кол-во кафедр]
+FROM dbo.FACULTY fc INNER JOIN dbo.PULPIT pp ON fc.FACULTY = pp.FACULTY
+GROUP BY fc.FACULTY_NAME
